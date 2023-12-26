510a637d09916854fc78fd63e7ce52ffc6e4e391
Nikita <nsalogub96@gmail.com>
2023-12-26 18:48:36 +0200

    try this

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -6,13 +6,13 @@ public class TicketBookingSystem {
     private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-        semaphore = new Semaphore(totalSeats);
+        semaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
         if (semaphore.tryAcquire()) {
-            return new BookingResult(user, true, "Booking successful");
+            return new BookingResult(user, true, "Booking successful.");
         }
-        return new BookingResult(user, false, "No seats available");
+        return new BookingResult(user, false, "No seats available.");
     }
 }
