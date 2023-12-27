afa118ff9f41ee31e07ee6916e9190d2f37ac6e5
Rostyslav Kuzmych <rostislavkuzmic401@gmail.com>
2023-12-27 14:44:00 +0200

    implemented the logic

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,26 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private int totalSeats;
+    private Semaphore semaphore = new Semaphore(totalSeats);
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        BookingResult result = null;
+        try {
+            semaphore.acquire();
+            result = new BookingResult(user, true, "You booked a seat!");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            result = new BookingResult(user, false, "There isn't an available seat! Try later.");
+        } finally {
+            semaphore.release();
+        }
+        return result;
     }
 }
