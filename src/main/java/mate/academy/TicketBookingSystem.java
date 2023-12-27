3b8aad5010c8a25ba25533ee51ad480f6b1a28ba
PavloSliepchenko <sliepchenkopavlo@gmail.com>
2023-12-27 04:51:46 -0600

    Task solution added

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,22 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final boolean SUCCESS = true;
+    private static final boolean FAIL = false;
+    private static final String SUCCESS_MESSAGE = "Booking successful.";
+    private static final String FAIL_MESSAGE = "No seats available.";
+    private Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, SUCCESS, SUCCESS_MESSAGE);
+        }
+        return new BookingResult(user, FAIL, FAIL_MESSAGE);
     }
 }
