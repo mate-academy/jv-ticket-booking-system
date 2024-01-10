e9186827c9a68846da4039da3db8822a426c423c
Nikita Akosnov <nikitaua4226@gmail.com>
2024-01-10 17:00:40 +0200

    implemented ticketBookingSystem

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,34 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
+    private int seatsAvailable;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
+        seatsAvailable = totalSeats;
     }
 
-    public BookingResult attemptBooking(String user) {
-        return null;
+    public BookingResult attemptBooking(String user) {    
+        try {
+            semaphore.acquire();
+            if (seatsAvailable > 0) {
+                decrementSeatsAvailable();
+                return new BookingResult(user, true, "Booking successful.");
+            } else {
+                return new BookingResult(user, false, "No seats available.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Thread was interrupted!", e);
+        } finally {
+            semaphore.release();
+        }
+    }
+
+    private synchronized void decrementSeatsAvailable() {
+        seatsAvailable--;
     }
 }
