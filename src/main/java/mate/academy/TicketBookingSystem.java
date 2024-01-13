b3072cf099117588134af866652f5c6fba974401
Kateryna Mykhailovska <mkhlvskate@gmail.com>
2024-01-14 01:43:53 +0200

    Requirements were implemented.

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,30 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private AtomicInteger counter;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
+        this.counter = new AtomicInteger(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (counter.get() > 0) {
+                counter.decrementAndGet();
+                return new BookingResult(user, true, "Booking successful.");
+            }
+            return new BookingResult(user, false, "No seats available.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Thread was interrupted!", e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
