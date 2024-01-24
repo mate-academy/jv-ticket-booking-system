59e35448585669a8443ed6ad3c8b80c04fa37d6d
HovorukhaBohdan <hovorukha.bohdan@gmail.com>
2024-01-24 14:20:22 +0200

    Implemented logic for TicketBookingSystem

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,31 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
+    private AtomicInteger numberOfSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
+        numberOfSeats = new AtomicInteger(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (numberOfSeats.get() > 0) {
+                numberOfSeats.decrementAndGet();
+                return new BookingResult(user, true, "Booking successful.");
+            }
+
+            return  new BookingResult(user, false, "No seats available.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
