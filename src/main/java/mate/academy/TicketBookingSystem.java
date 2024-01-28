ac0e46917005bd0922916510896fb723cea1526f
Mykhailo Kuryk <113907200+mishaa16@users.noreply.github.com>
2024-01-28 16:36:05 +0200

    add solution

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,31 @@
 package mate.academy;
 
+import java.util.concurrent.ForkJoinWorkerThread;
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private Integer totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
+        this.totalSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (totalSeats > 0) {
+                totalSeats--;
+                return new BookingResult(user, true, "Booking successful.");
+            } else {
+                return new BookingResult(user, false, "No seats available.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Thread was interrupted!", e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
