3a04fb997db01f3abb84dd4b471dbe0fc36987f5
VladSlob <141657806+VladSlob@users.noreply.github.com>
2024-04-08 07:44:43 +0300

    TicketBookingSystem.java

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -5,24 +5,16 @@ import java.util.concurrent.Semaphore;
 public class TicketBookingSystem {
 
     private final Semaphore availableSeats;
-    private final int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-        this.totalSeats = totalSeats;
         this.availableSeats = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        boolean acquired = availableSeats.tryAcquire();
-        try {
-            if (acquired) {
-
-                return new BookingResult(user, true, "Booking successful.");
-            } else {
-                return new BookingResult(user, false, "No seats available.");
-            }
-        } finally {
-
+        if (availableSeats.tryAcquire()) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
         }
     }
 }
