990ba6cab0f3999538be481e85b7bc04e4fa7991
Ostap Danyliv <danyliv.ostap@gmail.com>
2024-04-15 19:56:14 -0500

    ticket booking solution

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,21 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    public static final String BOOKING_SUCCESSFUL = "Booking successful.";
+    public static final String BOOKING_UNAVAILABLE = "No seats available.";
+    private final Semaphore semaphore;
 
+    public TicketBookingSystem(int totalSeats) {
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, true, BOOKING_SUCCESSFUL);
+        }
+        return new BookingResult(user, false, BOOKING_UNAVAILABLE);
     }
 }
