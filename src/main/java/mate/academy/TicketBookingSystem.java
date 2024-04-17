bb39aaad27759557a2e48d968e3af62b0d6f3cbe
Potseluiko Vitalii <vetaliy063@gmail.com>
2024-04-17 21:03:28 +0300

    realised attemptBooking method

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final String SUCCESS = "Booking successful.";
+    private static final String FAIL = "No seats available.";
+    private final Semaphore semaphore;
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            if (totalSeats > 0) {
+                totalSeats--;
+                return new BookingResult(user, true, SUCCESS);
+            }
+        }
+        return new BookingResult(user, false, FAIL);
     }
 }
