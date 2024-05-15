8d862fa2619801c3bd1225087a1832d6ff15ebbf
Stepan <oleksiukstepann@gmail.com>
2024-05-15 20:13:34 +0300

    Change implementation according to tryAcquire()

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -3,26 +3,22 @@ package mate.academy;
 import java.util.concurrent.Semaphore;
 
 public class TicketBookingSystem {
-    private final Semaphore semaphore = new Semaphore(1);
-    private int totalSeats;
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-        this.totalSeats = totalSeats;
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
         try {
-            semaphore.acquire();
-            if (totalSeats > 0) {
-                totalSeats--;
+            if (semaphore.tryAcquire()) {
                 return new BookingResult(user, true, "Booking successful.");
+            } else {
+                return new BookingResult(user, false, "No seats available.");
             }
-        } catch (InterruptedException e) {
+        } catch (RuntimeException e) {
             Thread.currentThread().interrupt();
-        } finally {
-            semaphore.release();
+            throw new RuntimeException(e);
         }
-
-        return new BookingResult(user, false, "No seats available.");
     }
 }
