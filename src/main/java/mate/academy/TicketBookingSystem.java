e8b112b5127eab92ade847144885d212705c3e0e
Stepan <oleksiukstepann@gmail.com>
2024-05-15 19:51:35 +0300

    Implement class TicketBookingSystem

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,28 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore = new Semaphore(1);
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (totalSeats > 0) {
+                totalSeats--;
+                return new BookingResult(user, true, "Booking successful.");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            semaphore.release();
+        }
+
+        return new BookingResult(user, false, "No seats available.");
     }
 }
