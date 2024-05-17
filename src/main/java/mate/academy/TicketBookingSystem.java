ddb6386563a7d9eb1505f34c14a2fb61f766e849
ilko-ilya <samilyakilya@gmail.com>
2024-05-17 21:50:02 +0300

    First commit

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,20 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean success = semaphore.tryAcquire();
+        if (success) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
