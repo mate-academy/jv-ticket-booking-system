65b6425704cad28d455b9d5a705f5d446de85dc6
marinakusniruk <Kushnirukmv@gmail.com>
2024-06-03 10:54:52 +0200

    Task completed

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,20 @@
 package mate.academy;
 
-public class TicketBookingSystem {
+import java.util.concurrent.Semaphore;
+
+public class TicketBookingSystem extends Thread {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
