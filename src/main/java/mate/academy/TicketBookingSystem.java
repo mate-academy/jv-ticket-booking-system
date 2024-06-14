ee4726d454a2a68f9416b47b6af80b733c356342
Anton Uzhva <anton.itec91@gmail.com>
2024-06-14 17:36:02 +0300

    Completed task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,29 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private volatile int totalSeats;
+    private Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (semaphore.tryAcquire()) {
+                synchronized (this) {
+                    totalSeats--;
+                }
+                return new BookingResult(user, true, "Booking successful.");
+            } else {
+                return new BookingResult(user, false, "No seats available.");
+            }
+        } finally {
+            if (semaphore.availablePermits() > totalSeats)
+                semaphore.release();
+        }
     }
 }
