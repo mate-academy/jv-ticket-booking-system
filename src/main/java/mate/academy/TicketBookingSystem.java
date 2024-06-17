c6faf01e05fc323abd4e8ed90d6a74ebf220aa2e
bodya4243 <bogusbogdan67@gmail.com>
2024-06-18 00:37:40 +0300

    1) completed hw

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,24 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean success = false;
+        String message;
+        if (semaphore.tryAcquire()) {
+            success = true;
+            message = "Booking successful.";
+        } else {
+            message = "No seats available.";
+        }
+
+        return new BookingResult(user, success, message);
     }
 }
