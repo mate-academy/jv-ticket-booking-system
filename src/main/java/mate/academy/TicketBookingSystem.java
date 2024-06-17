7d911a5c11952a31353aa80bd2718a7ebf8dbd17
Oleksandr Samoylenko <ntskazhu1985@gmail.com>
2024-06-17 17:06:36 +0300

    Solution was added

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,37 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private int seatsNumber;
+    private final Lock lock = new ReentrantLock(true);
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.seatsNumber = totalSeats;
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            lock.lock();
+            try {
+                if (seatsNumber > 0) {
+                    seatsNumber--;
+                    return new BookingResult(user, true, "Booking successful.");
+                } else {
+                    return new BookingResult(user, false, "No seats available.");
+                }
+            } finally {
+                lock.unlock();
+            }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
