1216e2625e5fd6fd52c0525f0cc1e166223267fe
Taras Fedak <tarasfedak@me.com>
2024-06-17 17:57:17 +0300

    Task completed.
    All test passed

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final String SUCCESS_MSG = "Booking successful.";
+    private static final String NO_TICKETS_AVAILABLE_MSG = "No seats available.";
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (semaphore.tryAcquire()) {
+                return new BookingResult(user, true, SUCCESS_MSG);
+            } else {
+                return new BookingResult(user, false, NO_TICKETS_AVAILABLE_MSG);
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException("attempt Booking failed ", t);
+        }
     }
 }
