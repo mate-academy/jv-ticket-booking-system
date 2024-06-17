cde4ee5a1e530df42f072c150cf2c2b6eb557cd3
HAIOVIY <gaevoywork@gmail.com>
2024-06-17 15:08:29 +0300

    add booking

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,24 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore seatsSemaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        seatsSemaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean acquired;
+        try {
+            acquired = seatsSemaphore.tryAcquire();
+            if (acquired) {
+                return new BookingResult(user, true, "Booking successful.");
+            }
+            return new BookingResult(user, false, "No seats available.");
+        } catch (Exception e) {
+            throw new RuntimeException("Error while attempting to book seats.", e);
+        }
     }
 }
