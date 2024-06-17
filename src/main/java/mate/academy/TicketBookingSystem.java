b0602a04e9d2677e5dea1a344f844aece5b84057
Nazar Klimovych <rksvjdbx@gmail.com>
2024-06-17 13:54:44 +0300

    Added solution for task ticket booking system

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,27 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final String SUCCESS_MESSAGE = "Booking successful.";
+    private static final String NO_SEATS_AVAILABLE_MESSAGE = "No seats available.";
+    private static final String EXECUTION_FAILURE_MESSAGE = "The execution was interrupted";
+
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (semaphore.tryAcquire()) {
+                return new BookingResult(user, true, SUCCESS_MESSAGE);
+            } else {
+                return new BookingResult(user, false, NO_SEATS_AVAILABLE_MESSAGE);
+            }
+        } catch (Exception e) {
+            return new BookingResult(user, false, EXECUTION_FAILURE_MESSAGE);
+        }
     }
 }
