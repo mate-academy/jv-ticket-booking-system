29611bbf26a81b5d4ba072ddce0893e8eef7f225
Pavlo-Home-PC <serikovpavel99@gmail.com>
2024-06-18 02:05:41 +0300

    feat : completed the task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
-
+    private static final String SUCCESS_MSG = "Booking successfull";
+    private static final String NO_TICKETS_AVAILABLE_MSG = "No tickets available";
+    private final Semaphore semaphore;
+    
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            if (semaphore.tryAcquire()) {
+                return new BookingResult(user, true, SUCCESS_MSG);
+            } else {
+                return new BookingResult(user, false, NO_TICKETS_AVAILABLE_MSG);
+            }
+        } catch (Throwable t) {
+            throw new RuntimeException("Error while attempting to book tickets", t);
+        }
     }
 }
