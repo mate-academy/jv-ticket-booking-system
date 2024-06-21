6cfafe1681a66a86ed883fbe876b5b32f34b774d
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-21 23:42:13 +0300

    added solution

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -15,14 +15,16 @@ public class TicketBookingSystem {
         BookingResult bookingResult = null;
         try {
             countSemaphore.acquire();
+            System.out.println("availableSeats = " + availableSeats);
             if (availableSeats == 0) {
                 bookingResult = new BookingResult(user, false, "No seats available.");
-                Thread.sleep(availableSeats);
+                Thread.sleep(availableSeats + 1);
             }
             if (availableSeats > 0) {
                 availableSeats--;
+
                 bookingResult = new BookingResult(user, true, "Booking successful.");
-                Thread.sleep(availableSeats);
+                Thread.sleep(availableSeats + 1);
             }
         } catch (InterruptedException e) {
             throw new RuntimeException("The execution was interrupted");
