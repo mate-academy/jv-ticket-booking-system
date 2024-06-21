da42072331acf9b2930a5192e2f3d57fc929f699
Yana Stepanova <yanna.stepanova@gmail.com>
2024-06-22 00:15:23 +0300

    right answer

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -4,33 +4,14 @@ import java.util.concurrent.Semaphore;
 
 public class TicketBookingSystem {
     private final Semaphore countSemaphore;
-    private int availableSeats;
 
     public TicketBookingSystem(int totalSeats) {
         countSemaphore = new Semaphore(totalSeats, true);
-        availableSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        BookingResult bookingResult = null;
-        try {
-            countSemaphore.acquire();
-            System.out.println("availableSeats = " + availableSeats);
-            if (availableSeats == 0) {
-                bookingResult = new BookingResult(user, false, "No seats available.");
-                Thread.sleep(availableSeats + 1);
-            }
-            if (availableSeats > 0) {
-                availableSeats--;
-
-                bookingResult = new BookingResult(user, true, "Booking successful.");
-                Thread.sleep(availableSeats + 1);
-            }
-        } catch (InterruptedException e) {
-            throw new RuntimeException("The execution was interrupted");
-        } finally {
-            countSemaphore.release();
-        }
-        return bookingResult;
+        return countSemaphore.tryAcquire()
+                ? new BookingResult(user, true, "Booking successful.")
+                : new BookingResult(user, false, "No seats available.");
     }
 }
