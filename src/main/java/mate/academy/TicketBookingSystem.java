736f98226a28d5b8d34425b6f1d7082479956429
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2024-08-21 00:33:35 +0200

    HW59-solution

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,34 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private static final String NO_AVAILABLE = "No seats available.";
+    private static final String AVAILABLE = "Booking successful.";
+    private Semaphore semaphore;
+    private int totalSeats;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        synchronized (this) {
+            if (totalSeats <= 0) {
+                return new BookingResult(user, false, NO_AVAILABLE);
+            } else {
+                try {
+                    semaphore = new Semaphore(totalSeats);
+                    totalSeats--;
+                    semaphore.acquire();
+                    return new BookingResult(user, true, AVAILABLE);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                    throw new RuntimeException(e);
+                } finally {
+                    semaphore.release();
+                }
+            }
+        }
     }
 }
