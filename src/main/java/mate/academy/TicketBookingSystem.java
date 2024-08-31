3f6432307393f4159fdd732b172e385329bb3fbe
Serhii Nazarenko <nasarenko82@gmail.com>
2024-08-30 22:37:57 -0600

    first commit

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,21 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        boolean isAcquired = semaphore.tryAcquire();
+        if (isAcquired) {
+            return new BookingResult(
+                        user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
