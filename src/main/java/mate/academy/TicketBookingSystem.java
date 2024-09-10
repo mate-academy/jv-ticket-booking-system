7b4c9f7466190b220bec98e361f2d9f95402ca4b
Evhen Timofeiev <timofeevzhenya2107@gmail.com>
2024-09-10 19:19:05 +0300

    solve the task

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,30 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+
+    public TicketBookingSystem(Semaphore semaphore) {
+        this.semaphore = semaphore;
+    }
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return new BookingResult(user, false, "The execution was interrupted");
+            }
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
+
 }
