d519a58ef28324a513ea4978979785db92632b23
Artem Klishch <artemklishch@gmail.com>
2024-10-18 19:14:08 +0300

    fix: should add semaphore logic

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,32 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final int totalSeats;
+
+    private int bookedSeats;
+
+    private final Semaphore semaphore = new Semaphore(1);
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.totalSeats = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            if (bookedSeats == totalSeats) {
+                Thread.currentThread().interrupt();
+                return new BookingResult(user, false, "No seats available.");
+            }
+            bookedSeats += 1;
+            return new BookingResult(user, true, "Booking successful.");
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException(e);
+        } finally {
+            semaphore.release();
+        }
     }
 }
