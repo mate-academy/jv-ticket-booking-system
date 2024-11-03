c6c3780b0db098005dbe1e78160d79b7842d5d89
D.Getmansky <d.getmansky@developmentaid.org>
2024-11-04 00:46:22 +0200

    the task is done

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,37 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
 
-    public TicketBookingSystem(int totalSeats) {
+    private final Semaphore semaphore;
 
+    private final AtomicInteger seatsAvailable;
+
+    public TicketBookingSystem(int totalSeats) {
+        semaphore = new Semaphore(totalSeats);
+        seatsAvailable = new AtomicInteger(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (seatsAvailable.get() == 0) {
+            return new BookingResult(user, false, "No seats available.");
+        }
+
+        try {
+            semaphore.acquire();
+        } catch (Exception e) {
+            Thread.currentThread().interrupt();
+            return new BookingResult(user, false, "Booking failed");
+        }
+
+        BookingResult bookingResult = new BookingResult(user, true, "Booking successful.");
+
+        seatsAvailable.getAndDecrement();
+
+        semaphore.release();
+
+        return bookingResult;
     }
 }
