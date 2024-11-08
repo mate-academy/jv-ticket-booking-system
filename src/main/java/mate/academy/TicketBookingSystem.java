26ce531560b9dd56900db5c75c67859b24368c47
bobik <oliferchukrivne@i.ua>
2024-11-09 00:59:02 +0200

    task done

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,23 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore seatsTotal;
 
     public TicketBookingSystem(int totalSeats) {
-
+        seatsTotal = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (seatsTotal.tryAcquire()) {
+            try {
+                return new BookingResult(user, true, "Booking successful.");
+            } finally {
+                seatsTotal.release();
+            }
+        } else {
+            return new BookingResult(user, false, "Cannot booking");
+        }
     }
 }
