303f39307d3d1ccb9b2edca6a47d61d43ddef470
ChabVlad <vlad.chabak@gmail.com>
2024-11-27 15:41:49 +0400

    done

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -10,14 +10,11 @@ public class TicketBookingSystem {
     }
 
     public BookingResult attemptBooking(String user) {
-        //System.out.println(user + "is trying to book a ticket");
         boolean success = semaphore.tryAcquire();
 
         if (success) {
-            //System.out.println(user + "booked a ticket");
             return new BookingResult(user, true, "Booking successful.");
         } else {
-            //System.out.println(user + " could not book a ticket. No seats available.");
             return new BookingResult(user, false, "No seats available.");
         }
     }
