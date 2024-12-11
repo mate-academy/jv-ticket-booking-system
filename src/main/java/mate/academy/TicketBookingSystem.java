5048e9814705cac000bea5631068ca919c525113
Oleksandr Kalenyk <carbonmail88@gmail.com>
2024-12-11 18:07:40 +0200

    jv-ticket-booking-system solution

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,25 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        if (totalSeats < 1) {
+            throw new IllegalArgumentException("totalSeats must be greater than 0");
+        }
+        semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (user == null || user.isEmpty()) {
+            throw new IllegalArgumentException("invalid user");
+        }
+        if (semaphore.tryAcquire()) {
+            return new BookingResult(user, true, "Booking successful.");
+        } else {
+            return new BookingResult(user, false, "No seats available.");
+        }
     }
 }
