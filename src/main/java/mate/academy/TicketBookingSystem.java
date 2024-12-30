dd8624f61d20957e8d4923305ea9821318b07b0a
iurii <kenu21@gmail.com>
2024-12-30 17:42:33 +0100

    SEMAPHORE: added semaphore to class

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,36 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TicketBookingSystem {
+    private AtomicInteger availableSeats;
+    private final Semaphore hall;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.hall = new Semaphore(totalSeats);
+        this.availableSeats = new AtomicInteger(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            hall.acquire();
+            if (availableSeats.get() > 0) {
+                BookingResult ticket =
+                        new BookingResult(user, true, "Booking successful.");
+                availableSeats.decrementAndGet();
+                hall.release();
+                return ticket;
+            } else {
+                BookingResult noTickets =
+                        new BookingResult(user, false, "No seats available.");
+                hall.release();
+                return noTickets;
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            hall.release();
+            throw new RuntimeException(e);
+        }
     }
 }
