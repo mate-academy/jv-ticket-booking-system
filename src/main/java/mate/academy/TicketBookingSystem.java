d13751c46319810d27b8d16cfe31ffd08664158c
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-02-18 10:50:24 +0100

    second try

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -6,12 +6,12 @@ public class TicketBookingSystem {
     private final Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-        this.semaphore = new Semaphore(totalSeats);
+        this.semaphore = new Semaphore(totalSeats, true);
     }
 
     public BookingResult attemptBooking(String user) {
         if (semaphore.tryAcquire()) {
-            return new BookingResult(user, true, "Booking successful!");
+            return new BookingResult(user, true, "Booking successful.");
         } else {
             return new BookingResult(user, false, "No seats available.");
         }
