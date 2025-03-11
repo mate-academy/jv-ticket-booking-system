b43e443dd22d350b185e3862794c01b397011708
svg <stelmaschyk.vg@gmail.com>
2025-03-11 13:11:23 +0200

    Complete task by semaphore functionality

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,27 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private int availableSeats;
+
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.availableSeats = totalSeats;
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        if (semaphore.tryAcquire()) {
+            synchronized (this) {
+                if (availableSeats > 0) {
+                    availableSeats--;
+                    semaphore.release();
+                    return new BookingResult(user, true, "Available Seats: " + availableSeats);
+                }
+            }
+        }
+        return new BookingResult(user, false, "Not enough seats");
     }
 }
