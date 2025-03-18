614e47b29843de61e74beb188d69dbd70af30cdf
Igor Neminuschii <igorgorslav@gmail.com>
2025-03-18 17:36:12 +0200

    implemented booking system

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,36 @@
 package mate.academy;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private final Semaphore semaphore;
+    private List<String> users;
+    private int tickets;
 
     public TicketBookingSystem(int totalSeats) {
-
+        semaphore = new Semaphore(totalSeats);
+        users = new ArrayList<>(totalSeats);
+        tickets = totalSeats;
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        BookingResult result;
+        if (semaphore.availablePermits() > 0 && users.size() < tickets) {
+            try {
+                semaphore.acquire();
+                Thread.currentThread().sleep(600);
+                result = new BookingResult(user, true, "Booking successful.");
+                users.add(user);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            } finally {
+                semaphore.release();
+            }
+        } else {
+            result = new BookingResult(user, false, "No seats available.");
+        }
+        return result;
     }
 }
