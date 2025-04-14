518be050bb07ead73c92a0c73535270a3e00ba73
Orest Sorokowey <osorokowey@gmail.com>
2025-04-14 15:13:56 +0300

    semaphore

--- src/main/java/mate/academy/TicketBookingSystem.java
+++ src/main/java/mate/academy/TicketBookingSystem.java
@@ -1,12 +1,20 @@
 package mate.academy;
 
+import java.util.concurrent.Semaphore;
+
 public class TicketBookingSystem {
+    private Semaphore semaphore;
 
     public TicketBookingSystem(int totalSeats) {
-
+        this.semaphore = new Semaphore(totalSeats);
     }
 
     public BookingResult attemptBooking(String user) {
-        return null;
+        try {
+            semaphore.acquire();
+            return new BookingResult(user, true, "Booking successful.");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
